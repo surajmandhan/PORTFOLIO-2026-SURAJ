c8c3bb89023fa61218225091d41e842964a0d21b
SURAJ <macsolutions@MACs-MacBook-Pro.local>
2026-03-19 09:42:34 +0530

    Fixed Vercel typescript build error in vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
 declare module 'gsap-trial/SplitText';
 declare module 'gsap-trial/ScrollSmoother';
+declare module 'gsap-trial/SplitText';
+declare module 'gsap-trial/ScrollSmoother';
